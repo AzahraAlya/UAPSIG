fe846a92d72ae75bd05c89a79f6a7dcf6916a0a3
DinaPutriAulia <nazaria20162016@gmail.com>
2021-07-13 14:55:38 +0700

    commit kesepuluh

--- application/views/template/head.php
+++ application/views/template/head.php
@@ -58,21 +58,21 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="<?= base_url('assets/AdminLTE-3.1.0/') ?>index3.html" class="brand-link">
-      <img src="<?= base_url('assets/AdminLTE-3.1.0/') ?>dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+<!-- <img src=dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8"> -->
+      <span class="brand-text font-weight-light">Tempat Wisata di  <br> Lampung</span>
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
       <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      <!-- <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="<?= base_url('assets/AdminLTE-3.1.0/') ?>dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src=" dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="<?= base_url('assets/AdminLTE-3.1.0/') ?>#" class="d-block">Alexander Pierce</a>
+          <a href=" #" class="d-block">Alexander Pierce</a>
         </div>
-      </div>
+      </div> -->
 
 
       <!-- Sidebar Menu -->
