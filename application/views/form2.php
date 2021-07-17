12507c6b31898dde5ac65579ecb28ffec810a130
jihan <jihancahya28@gmail.com>
2021-07-17 15:03:33 +0700

    commit tujuhbelas

--- application/views/form2.php
+++ application/views/form2.php
@@ -8,15 +8,15 @@
                 <div class="col-md-6">
                     <div class="card">
                         <div class="card-header">
-                            <form method ="POST" action="<?php echo base_url('Tamp/proses_edit_data') ?>">
-                            <input type="hidden" class="form-control" name="id_lokasi" id="id_lokasi">
+                        <form method ="POST" action="<?php echo base_url('Tamp/proses_edit_data') ?>">
+                            <input type="hidden" name="id_lokasi" value="<?php echo $data['id_lokasi'];?>">
                                 <div class="form-group">
-                                    <label>nama</label>
-                                    <input type="text" class="form-control" name="nama" id="nama" placeholder="nama">
+                                    <label for="nama" class="col-sm-2 col-form-label">nama</label>
+                                    <input type="text" class="form-control" name="nama"  value="<?php echo $data['nama'];?>">
                                 </div>
                                 <div class="form-group">
                                     <label>koordinat</label>
-                                    <input type="text" class="form-control" name="coord" id="coord"placeholder="koordinat lokasi">
+                                    <input type="text" class="form-control" name="coord" id="coord" value="<?php echo $data['coord'];?>">
                                 </div>
                                     <button type="submit" class="btn btn-primary">Submit</button>
                             </form>
